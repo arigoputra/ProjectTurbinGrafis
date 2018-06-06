a27956d58e86cf21921e1e58f49e20a1f7d1279d
novansayogas <no0vansatria@gmail.com>
2018-06-06 11:44:26 +0700

    Gear turbin awal

---
+++ Final_Project/src/org/yourorghere/Final_Project.java
@@ -0,0 +1,31 @@
+package org.yourorghere;
+
+import java.applet.*;
+import java.awt.*;
+import java.io.*;
+
+import javax.media.opengl.*;
+import com.sun.opengl.util.*;
+import org.yourorghere.Object;
+
+public class Final_Project extends Applet {
+  private Animator animator;
+
+  public void init() {
+    setLayout(new BorderLayout());
+    GLCanvas canvas = new GLCanvas();
+    canvas.addGLEventListener(new Object());
+    canvas.setSize(getSize());
+    add(canvas, BorderLayout.CENTER);
+    animator = new FPSAnimator(canvas, 60);
+  }
+
+  public void start() {
+    animator.start();
+  }
+
+  public void stop() {
+    // FIXME: do I need to do anything else here?
+    animator.stop();
+  }
+}
